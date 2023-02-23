1074bb0522a487fdcdd71e886fa767f58a38e90a
Alexandre Maheux <alexandre.maheux@ens.uvsq.fr>
2023-02-23 12:24:03 +0100

    Arrange indentation and space

--- genseq.c
+++ genseq.c
@@ -1,46 +1,35 @@
-//
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-//
 unsigned randxy(unsigned x, unsigned y)
 {
-  return (x + (rand() % y)) % y; 
+    return (x + (rand() % y)) % y; 
 }
 
-//
 int main(int argc, char **argv)
 {
-  //
-  srand(getpid());
+    srand(getpid());
   
-  //
-  if (argc < 2)
-    return printf("Usage: %s [output file] [length]\n", argv[0]), 1;
+    if(argc < 2)
+        return printf("Usage: %s [output file] [length]\n", argv[0]), 1;
 
-  //
-  unsigned long long len = atoll(argv[2]);
+    unsigned long long len = atoll(argv[2]);
   
-  //
-  const char bases[4] = "ATCG"; 
+    const char bases[4] = "ATCG"; 
   
-  //
-  FILE *fp = fopen(argv[1], "wb");
+    FILE *fp = fopen(argv[1], "wb");
+    if(!fp)
+        return printf("Error: cannot create file '%s'\n", argv[2]), 2;
 
-  if (!fp)
-    return printf("Error: cannot create file '%s'\n", argv[2]), 2;
+    //Generate random DNA sequence
+    for(unsigned long long i = 0; i < len; ++i)
+        fprintf(fp, "%c", bases[randxy(0, 4)]);
 
-  //Generate random DNA sequence
-  for (unsigned long long i = 0; i < len; i++)
-    fprintf(fp, "%c", bases[randxy(0, 4)]);
-
-  //Newline at EOF
-  fprintf(fp, "\n");
+    //Newline at EOF
+    fprintf(fp, "\n");
   
-  //
-  fclose(fp);
+    fclose(fp);
     
-  //
-  return 0;
+    return 0;
 }
